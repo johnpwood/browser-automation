5a9fb9f89db9ac7358ad013cb1145ebf424f4576
John Wood <johnpwoodiv@gmail.com>
2018-02-08 08:17:04 -0600

    start server and client

--- server/src/app.jsx
+++ server/src/app.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const form = (p) => {
+  return (
+      <form>
+        <input type="text" name="firstname" />
+      </form>;
+      )
+}
+
 ReactDOM.render(<p>This will be a form!</p>, document.getElementById('app'));
 
 
